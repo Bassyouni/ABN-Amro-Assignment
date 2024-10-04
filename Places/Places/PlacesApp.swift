fc489159f886656253e15b9f9865bdd86eb1066b
Omar Ashraf El-Bassiony <omae.bassyouni991@gmail.com>
2024-10-04 21:38:27 +0200

    add new empty place apps

---
+++ Places/Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Omar Bassyouni on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
