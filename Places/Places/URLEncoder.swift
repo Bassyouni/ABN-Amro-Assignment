05c2f9b96fd693b51238780eff2d7c7f420fcf9c
Omar Ashraf El-Bassiony <omae.bassyouni991@gmail.com>
2024-10-10 10:25:35 +0200

    add name to url if the place has one

--- Places/Places/URLEncoder.swift
+++ Places/Places/URLEncoder.swift
@@ -9,7 +9,13 @@ import Foundation
 
 public struct URLEncoder: PlacesURLEncoder {
     public func encodeWikipediaURL(latitude: Double, longitude: Double, name: String?) -> URL? {
-        let encodedURL = "wikipedia://places?WMFArticleURL=https%3A%2F%2Fen.wikipedia.org%2Fwiki%3Flatitude%3D\(latitude)%26longitude%3D\(longitude)"
+        var nameComponent: String = ""
+        
+        if let name = name {
+            nameComponent = "%2F\(name)"
+        }
+        
+        let encodedURL = "wikipedia://places?WMFArticleURL=https%3A%2F%2Fen.wikipedia.org%2Fwiki\(nameComponent)%3Flatitude%3D\(latitude)%26longitude%3D\(longitude)"
         
         return URL(string: encodedURL)
     }
--- Places/PlacesTests/URLEncoderTests.swift
+++ Places/PlacesTests/URLEncoderTests.swift
@@ -20,6 +20,16 @@ class URLEncoderTests: XCTestCase {
         let expectedURLString = "wikipedia://places?WMFArticleURL=https%3A%2F%2Fen.wikipedia.org%2Fwiki%3Flatitude%3D-33.8688%26longitude%3D151.2093"
         XCTAssertEqual(encodedURL?.absoluteString, expectedURLString)
     }
+    
+    func test_encodeWikipediaURL_withCoordinatesAndName_returnsCorrectlyEncodedURL() {
+        let sut = makeSUT()
+        let name = "Amsterdam"
+        
+        let encodedURL = sut.encodeWikipediaURL(latitude: 1, longitude: 1.2, name: name)
+        
+        let expectedURLString = "wikipedia://places?WMFArticleURL=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FAmsterdam%3Flatitude%3D1.0%26longitude%3D1.2"
+        XCTAssertEqual(encodedURL?.absoluteString, expectedURLString)
+    }
 }
 
 extension URLEncoderTests {
