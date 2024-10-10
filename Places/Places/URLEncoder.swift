97e51494367a8a1c3474a5f8c0a0703ec9007b07
Omar Ashraf El-Bassiony <omae.bassyouni991@gmail.com>
2024-10-10 10:02:39 +0200

    add basic `URLEncoder` implmentation

---
+++ Places/Places/URLEncoder.swift
@@ -0,0 +1,16 @@
+//
+//  URLEncoder.swift
+//  Places
+//
+//  Created by Omar Bassyouni on 09/10/2024.
+//
+
+import Foundation
+
+public struct URLEncoder: PlacesURLEncoder {
+    public func encodeWikipediaURL(latitude: Double, longitude: Double) -> URL? {
+        let encodedURL = "wikipedia://places?WMFArticleURL=https%3A%2F%2Fen.wikipedia.org%2Fwiki%3Flatitude%3D\(latitude)%26longitude%3D\(longitude)"
+        
+        return URL(string: encodedURL)
+    }
+}
---
+++ Places/PlacesTests/URLEncoderTests.swift
@@ -0,0 +1,29 @@
+//
+//  URLEncoderTests.swift
+//  Places
+//
+//  Created by Omar Bassyouni on 09/10/2024.
+//
+
+import XCTest
+@testable import Places
+
+class URLEncoderTests: XCTestCase {
+    
+    func test_encodeWikipediaURL_withNegativeCoordinates_returnsCorrectlyEncodedURL() {
+        let sut = makeSUT()
+        let latitude = -33.8688
+        let longitude = 151.2093
+        
+        let encodedURL = sut.encodeWikipediaURL(latitude: latitude, longitude: longitude)
+        
+        let expectedURLString = "wikipedia://places?WMFArticleURL=https%3A%2F%2Fen.wikipedia.org%2Fwiki%3Flatitude%3D-33.8688%26longitude%3D151.2093"
+        XCTAssertEqual(encodedURL?.absoluteString, expectedURLString)
+    }
+}
+
+extension URLEncoderTests {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLEncoder {
+        return URLEncoder()
+    }
+}
